7727822fa35492dc95864b03ff59b55ae5721f63
abdul.ashif <abdul.ashif@dot.abudhabi.ae>
2020-02-12 13:01:57 +0400

    Initial Commit

---
+++ AirplayDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AirplayDemo
+//
+//  Created by DOT Developer DOT on 12/02/2020.
+//  Copyright © 2020 ashif. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
